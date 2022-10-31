6339e0fcc4b2dd60d65bd929f44c68f97750c1b0
KaoSon2004 <gsu0987q@gmail.com>
2022-10-31 17:03:12 +0700

    header search bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,22 @@
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
 
+import Search from "../Search";
+
+const cx = classNames.bind(styles);
 function Header() {
-    return (
-        <div>
-            Header
-        </div>
-    )
+  return (
+    <div className={cx("wrapper")}>
+      <div className={cx("search")}>
+        <Search />
+      </div>
+
+      <div className={cx("user")}>
+        <p className={cx('user-name')}>anhquan7826</p>
+        <img className={cx('user-avatar')} src="https://t4.ftcdn.net/jpg/02/51/95/53/360_F_251955356_FAQH0U1y1TZw3ZcdPGybwUkH90a3VAhb.jpg"></img>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Layouts/DefaultLayout/DefaultLayout.js
+++ src/components/Layouts/DefaultLayout/DefaultLayout.js
@@ -1,13 +1,15 @@
-import Sidebar from "../../Sidebar"
 import classNames from "classnames/bind"
 import styles from './DefaultLayout.module.scss'
 
+import Sidebar from "../../Sidebar"
+import Header from '../../Header'
 const cx = classNames.bind(styles)
 function DefaultLayout({children}) {
     return (
         <div className={cx('wrapper')}>
             <Sidebar />
             <div className={cx('container')}>
+                <Header></Header>
                 {children}
             </div>
         </div>

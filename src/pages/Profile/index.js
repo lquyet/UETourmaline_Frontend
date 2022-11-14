a7061f5322916b6f86b968a18d2844f04e68ee17
Jo3yVX <anhquyethgvx@gmail.com>
2022-11-15 00:10:16 +0700

    core logics

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,8 +1,25 @@
-import className from 'classnames/bind';
+import className from "classnames/bind";
+import { useState, useEffect } from "react";
 
 function Profile() {
-    return (
-        <h1>Profile</h1>
-    )
+	const [userInfo, setUserInfo] = useState({});
+
+	useEffect(() => {
+		getUserInfo();
+	}, []);
+
+	const getUserInfo = async () => {
+		// Fetch API to get account information: username, email, password (?), phone number, address. (JSON)
+		// Hardcode for now
+		const response = {
+			username: "admin",
+			email: "admin@tourmaline.com",
+			phone: "0123456789",
+			address: "123, ABC Street, XYZ City, 12345",
+		};
+		setUserInfo(response);
+	}
+
+    return <h1>Profile</h1>;
 }
-export default Profile
+export default Profile;

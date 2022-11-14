565724395060fc5f2591951206c35d702de757e8
quyendv <daovanquyen45@gmail.com>
2022-11-15 01:29:40 +0700

    format all, config npm run pretty, auto format before commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import {publicRoutes} from './routes'
-import { Fragment } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DefaultLayout from './layouts/DefaultLayout';
+import { publicRoutes } from './routes';
 
 function App() {
-  return (
-    <div className="App">
-        <BrowserRouter>
-          <Routes>
-            {publicRoutes.map((route, index)=> {
-              const Page = route.page
-              let Layout;
-              if(route.layout !== null) {
-                Layout = route.layout
-              } else  {
-                Layout = DefaultLayout
-              }
-              return (
-                <Route key={index} path={route.path} element={
-                  <Layout>
-                    <Page />  
-                  </Layout>
-                } />
-              )
-            })}
-          </Routes>
-        </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.page;
+                        let Layout;
+                        if (route.layout !== null) {
+                            Layout = route.layout;
+                        } else {
+                            Layout = DefaultLayout;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Sidebar/SidebarItem/SidebarItem.js
+++ src/components/Sidebar/SidebarItem/SidebarItem.js
@@ -1,22 +1,22 @@
-import classNames from "classnames/bind";
-import { NavLink } from "react-router-dom";
-import styles from "./SidebarItem.module.scss";
-import PropTypes from 'prop-types'
+import classNames from 'classnames/bind';
+import { NavLink } from 'react-router-dom';
+import styles from './SidebarItem.module.scss';
+import PropTypes from 'prop-types';
 
 const cx = classNames.bind(styles);
 function SidebarItem({ icon, textContent, to }) {
-  return (
-    <NavLink  className={(nav) => cx('wrapper', {active: nav.isActive})} to={to} end>
-      <span className={cx('icon')}>{icon}</span>
-      <span className={cx('text-content')}>{textContent}</span>
-    </NavLink>
-  );
+    return (
+        <NavLink className={(nav) => cx('wrapper', { active: nav.isActive })} to={to} end>
+            <span className={cx('icon')}>{icon}</span>
+            <span className={cx('text-content')}>{textContent}</span>
+        </NavLink>
+    );
 }
 
 SidebarItem.propTypes = {
-  icon: PropTypes.node,
-  textContent: PropTypes.string,
-  to: PropTypes.string,
-}
+    icon: PropTypes.node,
+    textContent: PropTypes.string,
+    to: PropTypes.string,
+};
 
 export default SidebarItem;

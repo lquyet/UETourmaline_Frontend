f88b1eaff5b40ceab1f5d6244c2cee7931a0b14f
Jo3yVX <anhquyethgvx@gmail.com>
2022-11-17 11:50:49 +0700

    init strc

---
+++ src/components/PlayingBar/PlayingBar.js
@@ -0,0 +1,5 @@
+function PlayingBar() {
+    return <div></div>;
+}
+
+export default PlayingBar;
--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,9 +1,9 @@
 import classNames from 'classnames/bind';
 import styles from './DefaultLayout.module.scss';
-
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
 import { Outlet } from 'react-router-dom';
+import PlayingBar from '~/components/PlayingBar';
 const cx = classNames.bind(styles);
 function DefaultLayout() {
     return (
@@ -11,6 +11,7 @@ function DefaultLayout() {
             <Sidebar />
             <div className={cx('container')}>
                 <Header></Header>
+                <PlayingBar></PlayingBar>
                 <Outlet />
             </div>
         </div>
